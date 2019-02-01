586dd08c5e07bdd69a5264e50a852eb002b6c2ad
Valdeci Rolim <rolim.valdeci@gmail.com>
2019-01-31 21:11:42 -0300

    criada e testada a primeira classe de domínio trasendo via requisição os objetos como json

--- src/main/java/br/com/rolimvaldeci/cursomc/resources/CategoriaRescource.java
+++ src/main/java/br/com/rolimvaldeci/cursomc/resources/CategoriaRescource.java
@@ -1,17 +1,28 @@
 package br.com.rolimvaldeci.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rolimvaldeci.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaRescource {
 	
 	@GetMapping("/teste.json")
-	public String listar() {
-		return "O rest está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
 	}
 	
 }
