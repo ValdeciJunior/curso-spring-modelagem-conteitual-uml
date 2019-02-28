52f3027c75b7a5d69d012cd5dabf86570f8ff3d8
Valdeci Rolim Junior <valdeci@dimensiva.online>
2019-02-28 08:08:23 -0300

    inserido validação de cpf e cnpj para clientes

---
+++ src/main/java/br/com/rolimvaldeci/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package br.com.rolimvaldeci.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import br.com.rolimvaldeci.cursomc.domain.enums.TipoCliente;
+import br.com.rolimvaldeci.cursomc.dto.ClienteNewDTO;
+import br.com.rolimvaldeci.cursomc.resources.exceptions.FieldMessage;
+import br.com.rolimvaldeci.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		// inclua os testes aqui, inserindo erros na lista
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfCnpj())) {
+			list.add(new FieldMessage("cpfCnpj", "Cpf inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfCnpj())) {
+			list.add(new FieldMessage("cpfCnpj", "Cnpj inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
