3314b5fbbe21925a4ce5e0bcfa1ef2d39765154f
Valdeci Rolim Junior <valdeci@dimensiva.online>
2019-02-28 08:22:26 -0300

    criado validação que não permite cadastrar emails repetidos

--- src/main/java/br/com/rolimvaldeci/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/rolimvaldeci/cursomc/services/validation/ClienteInsertValidator.java
@@ -5,12 +5,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import br.com.rolimvaldeci.cursomc.domain.enums.TipoCliente;
 import br.com.rolimvaldeci.cursomc.dto.ClienteNewDTO;
+import br.com.rolimvaldeci.cursomc.repositories.ClienteRepository;
 import br.com.rolimvaldeci.cursomc.resources.exceptions.FieldMessage;
 import br.com.rolimvaldeci.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +34,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj", "Cnpj inválido"));
+		}
+		
+		if(repo.findByEmail(objDto.getEmail()) != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
